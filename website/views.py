d0bf839c3bdb079066fefefb5debd43f1c727466
ahmedalaatalaat <ahmedalaatalaat@gmail.com>
2020-06-07 01:37:08 +0200

    website is added

--- website/views.py
+++ website/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse
 # Create your views here.
+
+
+def base(request):
+    return render(request, "website/base.html")
+
+
+def Index(request):
+    return render(request, "website/index.html")
+    # return HttpResponse("<H1>Home Page</H1>")
